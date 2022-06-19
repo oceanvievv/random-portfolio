ce9e4e6f96452fbc78d17fdff6cc5a07cf17bdb7
oceanvievv <oceanvievv@gmail.com>
2022-06-20 02:46:09 +0500

    Change paths

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import Post from "./js/Post";
-import './styles/styles.css'
-import PortfolioPFP from './assets/images/photo_2022-06-17_23-16-17.jpg'
+import Post from "@/js/Post";
+import '@styles/styles.css'
+import PortfolioPFP from '@images/photo_2022-06-17_23-16-17.jpg'
 
 const post = new Post("Traxat...'", PortfolioPFP)
-console.log(post.getData());
+console.log(post)
