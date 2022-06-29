067abcb818d9a3a6a98c6357f66b89bac6e2c0c3
oceanvievv <oceanvievv@protonmail.com>
2022-06-29 22:27:36 +0500

    Remove unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 import './babel'
 import '@styles/index.scss'
 import { buttonHandler } from '@js/projects/filterButton'
-import generateProjects  from '@js/projects/projects'
-
-const projects = generateProjects(1)
